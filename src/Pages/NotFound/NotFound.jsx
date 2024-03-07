8ca2d3af765dcd8dd4b51d23543e8ecbd49703a5
Thomas Campanholi <thomas.campanholi@sitel.com>
2024-03-07 10:17:38 -0300

    adicao de estilos na tela de notfound

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { NotFoundContainer, NotFoundDiv } from './NotFound.styles.jsx';
+import Error from '../../Components/Images/NotFound/NotFound.svg'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <NotFoundContainer>
+      <NotFoundDiv>
+        <div>
+          <img src={Error} alt="checkboxSVG" />
+          <h1>Erro 404</h1>
+          <p>Ops! Essa página não existe.</p>
+          <Link to="/">
+            <button>Voltar</button>
+          </Link>
+        </div>
+      </NotFoundDiv>
+    </NotFoundContainer>
   )
 }
